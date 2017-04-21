8cb01f3d24b5b9e4783cdc3b9a34186423222f68
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-04-21 15:27:36 -0400

    All three pages setup.  Dropdown on 'cart' needs work.  More styling added.

--- public/cart.js
+++ public/cart.js
@@ -72,9 +72,52 @@ function getTotal() {
     request.send();
 }
 
+function getTables() {
+    let request = new XMLHttpRequest();
+        request.open('GET', 'http://tiy-28202.herokuapp.com/order');
+        request.addEventListener('load', function(){
+
+            let response = JSON.parse(request.responseText);
+            console.log(response);
+
+                let selection = document.querySelector('.tableID');
+                selection.addEventListener('change', function(){
+
+                    let showPayment = document.querySelector('.payment');
+                    showPayment.style.display = "block";
+
+                    getTotal();
+                    getBill();
+
+                });
+
+                for(let i = 0; i < response.length; i++){
+                            addTable(response[i].table_id);
+                };
+                
+            });
+
+        request.send();
+
+
+}
+
+function addTable(table_id){
+    let dropdown = document.querySelector('#dropdown-template').innerHTML;
+    let parent = document.querySelector('.tableID');
+    let container = document.createElement('option');
+    container.innerHTML = Mustache.render(dropdown, {
+        tableName: table_id,
+        });
+
+    parent.appendChild(container); 
+
+}
+
 
 window.addEventListener('load', function() {
-    getTotal();
-    getBill();
+    getTables();
+    // getTotal();
+    // getBill();
 });
 },{}]},{},[1]);
